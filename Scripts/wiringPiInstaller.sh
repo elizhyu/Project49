5d8920872c3b672fd59f1f71af82fbe91066f4b0
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-02-09 00:25:04 -0500

    Create wiringPiInstaller.sh

---
+++ Scripts/wiringPiInstaller.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#This script installs wiringPi
+
+#In case git isn't installed
+sudo apt-get install git-core
+
+cd
+git clone git://git.drogon.net/wiringPi
+
+cd ~/wiringPi
+git pull origin
+
+cd~/wiringPi
+./build
