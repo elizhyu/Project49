1c0b798c91ceaee1437f9a29b18b893a879c00db
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-02-12 18:04:06 -0500

    Update TestScript.sh

--- Scripts/TestScript.sh
+++ Scripts/TestScript.sh
@@ -6,10 +6,18 @@ cd Code
 cd -B
 #./main
 
+#doesn't work
+declare -i time_left
+
 for i in {1..10}
 do
+  #doesn't work
+  time_left = 10-#i
+  echo Recording: $i/10
   #touch hooks/start_record
   sleep $1
-  
-  echo $time_left
+  #touch hooks/stop_record
+  sleep 2
+  echo Time remaining:
 done
+echo Done
