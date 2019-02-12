b90da8ceebc90e4ddd7c8555e9fa8382f3de8dd7
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-02-12 17:09:48 -0500

    Update Project49Installer.sh

--- Scripts/Project49Installer.sh
+++ Scripts/Project49Installer.sh
@@ -3,11 +3,14 @@
 #This is the script that runs all the installations and sets everything up
 
 echo "Running the main update/upgrade"
+chmod +x mainUpdater.sh
 ./mainUpdater.sh
 
 echo "Installing wiringPi"
+chmod +x wiringPiInstaller.sh
 ./wiringPiInstaller.sh
 
 echo "Installing picam"
+chmod +x piCamInstaller.sh
 ./piCamInstaller.sh
 
