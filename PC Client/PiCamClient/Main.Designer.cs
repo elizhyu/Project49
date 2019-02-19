6f912a1e46650a3792e2b15fa379d9db34c34bed
Eli Yu <31944193+elizhyu@users.noreply.github.com>
2019-02-19 16:12:40 -0500

    Add PC Client Code

---
+++ PC Client/PiCamClient/Main.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace PiCamClient
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Button_Transfer = new System.Windows.Forms.Button();
+            this.PiCam_List = new System.Windows.Forms.ComboBox();
+            this.Transfer_Progress_List = new System.Windows.Forms.ListView();
+            this.Button_Connect = new System.Windows.Forms.Button();
+            this.Status_Refresh_Timer = new System.Windows.Forms.Timer(this.components);
+            this.Button_Record = new System.Windows.Forms.Button();
+            this.Player_1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Button_Test = new System.Windows.Forms.Button();
+            this.Button_Disconnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button_Transfer
+            // 
+            this.Button_Transfer.Location = new System.Drawing.Point(659, 39);
+            this.Button_Transfer.Name = "Button_Transfer";
+            this.Button_Transfer.Size = new System.Drawing.Size(75, 23);
+            this.Button_Transfer.TabIndex = 1;
+            this.Button_Transfer.Text = "Transfer";
+            this.Button_Transfer.UseVisualStyleBackColor = true;
+            this.Button_Transfer.Click += new System.EventHandler(this.Button_Transfer_Click);
+            // 
+            // PiCam_List
+            // 
+            this.PiCam_List.FormattingEnabled = true;
+            this.PiCam_List.Items.AddRange(new object[] {
+            "Eli_HA (Test Only)",
+            "PiCam 1"});
+            this.PiCam_List.Location = new System.Drawing.Point(418, 12);
+            this.PiCam_List.Name = "PiCam_List";
+            this.PiCam_List.Size = new System.Drawing.Size(154, 21);
+            this.PiCam_List.TabIndex = 0;
+            this.PiCam_List.SelectedIndexChanged += new System.EventHandler(this.PiCam_List_SelectedIndexChanged);
+            // 
+            // Transfer_Progress_List
+            // 
+            this.Transfer_Progress_List.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.Transfer_Progress_List.Location = new System.Drawing.Point(418, 68);
+            this.Transfer_Progress_List.Name = "Transfer_Progress_List";
+            this.Transfer_Progress_List.Size = new System.Drawing.Size(316, 370);
+            this.Transfer_Progress_List.TabIndex = 6;
+            this.Transfer_Progress_List.UseCompatibleStateImageBehavior = false;
+            // 
+            // Button_Connect
+            // 
+            this.Button_Connect.Location = new System.Drawing.Point(578, 10);
+            this.Button_Connect.Name = "Button_Connect";
+            this.Button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.Button_Connect.TabIndex = 7;
+            this.Button_Connect.Text = "Connect";
+            this.Button_Connect.UseVisualStyleBackColor = true;
+            this.Button_Connect.Click += new System.EventHandler(this.Button_Connect_Click);
+            // 
+            // Status_Refresh_Timer
+            // 
+            this.Status_Refresh_Timer.Enabled = true;
+            this.Status_Refresh_Timer.Interval = 300;
+            this.Status_Refresh_Timer.Tick += new System.EventHandler(this.Status_Refresh_Timer_Tick);
+            // 
+            // Button_Record
+            // 
+            this.Button_Record.Location = new System.Drawing.Point(578, 39);
+            this.Button_Record.Name = "Button_Record";
+            this.Button_Record.Size = new System.Drawing.Size(74, 22);
+            this.Button_Record.TabIndex = 10;
+            this.Button_Record.Text = "Record";
+            this.Button_Record.UseVisualStyleBackColor = true;
+            this.Button_Record.Click += new System.EventHandler(this.Button_Record_Click);
+            // 
+            // Player_1
+            // 
+            this.Player_1.Enabled = true;
+            this.Player_1.Location = new System.Drawing.Point(12, 12);
+            this.Player_1.Name = "Player_1";
+            this.Player_1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player_1.OcxState")));
+            this.Player_1.Size = new System.Drawing.Size(400, 225);
+            this.Player_1.TabIndex = 3;
+            this.Player_1.TabStop = false;
+            this.Player_1.Enter += new System.EventHandler(this.Player_1_Enter);
+            // 
+            // Button_Test
+            // 
+            this.Button_Test.Location = new System.Drawing.Point(498, 39);
+            this.Button_Test.Name = "Button_Test";
+            this.Button_Test.Size = new System.Drawing.Size(74, 22);
+            this.Button_Test.TabIndex = 11;
+            this.Button_Test.Text = "Test";
+            this.Button_Test.UseVisualStyleBackColor = true;
+            this.Button_Test.Click += new System.EventHandler(this.Button_Test_Click);
+            // 
+            // Button_Disconnect
+            // 
+            this.Button_Disconnect.Enabled = false;
+            this.Button_Disconnect.Location = new System.Drawing.Point(659, 10);
+            this.Button_Disconnect.Name = "Button_Disconnect";
+            this.Button_Disconnect.Size = new System.Drawing.Size(75, 23);
+            this.Button_Disconnect.TabIndex = 12;
+            this.Button_Disconnect.Text = "Disconnect";
+            this.Button_Disconnect.UseVisualStyleBackColor = true;
+            this.Button_Disconnect.Click += new System.EventHandler(this.Button_Disconnect_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 450);
+            this.Controls.Add(this.Button_Disconnect);
+            this.Controls.Add(this.Button_Test);
+            this.Controls.Add(this.Button_Record);
+            this.Controls.Add(this.Button_Connect);
+            this.Controls.Add(this.Transfer_Progress_List);
+            this.Controls.Add(this.PiCam_List);
+            this.Controls.Add(this.Player_1);
+            this.Controls.Add(this.Button_Transfer);
+            this.Name = "Main";
+            this.Text = "PiCam PC Client";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.Player_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Button_Transfer;
+        private AxWMPLib.AxWindowsMediaPlayer Player_1;
+        private System.Windows.Forms.ComboBox PiCam_List;
+        private System.Windows.Forms.ListView Transfer_Progress_List;
+        private System.Windows.Forms.Button Button_Connect;
+        private System.Windows.Forms.Timer Status_Refresh_Timer;
+        private System.Windows.Forms.Button Button_Record;
+        private System.Windows.Forms.Button Button_Test;
+        private System.Windows.Forms.Button Button_Disconnect;
+    }
+}
+
---
+++ PC Client/PiCamClient/Main.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinSCP;
+using WMPLib;
+using System.Resources;
+
+namespace PiCamClient
+{
+    public partial class Main : Form
+    {
+        SessionOptions Eli_HA = new SessionOptions
+        {
+            Protocol = Protocol.Sftp,
+            HostName = "172.16.0.183",
+            UserName = "root",
+            Password = "yu111333",
+            SshHostKeyFingerprint = "ssh-ed25519 256 40:b0:cb:19:3a:fc:b7:98:cd:b2:a3:58:2a:b3:ae:8c"
+        };
+
+        SessionOptions PiCam_1 = new SessionOptions
+        {
+            Protocol = Protocol.Sftp,
+            HostName = "192.168.1.11",
+            UserName = "pi",
+            Password = "raspberry",
+            SshHostKeyFingerprint = "ssh-ed25519 256 b6:89:da:69:bb:96:7c:e7:12:83:7e:79:f8:96:2c:ba"//"ssh-ed25519 256 b0:2c:21:d8:bd:e4:c8:c3:9d:b9:2a:a4:90:06:c5:8a"
+        }; 
+
+         // Transfer Progress List Groups
+         ListViewGroup Initiation_Group = new ListViewGroup("Live Monitor");
+        ListViewGroup Transfer_Group = new ListViewGroup("File Transfer");
+
+        string[] File_List;
+        bool file_list_state = false;
+
+        // Winscp Result Declaration
+        CommandExecutionResult execute_result;
+
+        // Background Stop Flag
+        bool status_check_stop_flag = false;
+
+        // 
+        string status = "off";
+        string action = "none";
+        bool recording_status = false;
+        int PiCam_Selection = -1;
+        bool transfer_status = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Transfer_Click(object sender, EventArgs e)
+        {
+            action = "transfer";
+        }
+
+        private void SessionFileTransferProgress(object sender , FileTransferProgressEventArgs e)
+        {
+            string _lastFileName = "";
+            int currentindex = 0;
+            // Grab file name
+            string[] filename_array = e.FileName.Split('/');
+
+            // New line for every new file
+            if (_lastFileName != e.FileName)
+            {
+                foreach(ListViewItem theitem in Transfer_Progress_List.Items)
+                {
+                    if (theitem.SubItems[0].Text.Split('(')[0] == filename_array[filename_array.Length - 1])
+                    {
+                        currentindex = theitem.Index;
+                        Transfer_Progress_List.Items[currentindex].EnsureVisible();
+                    }
+                }
+            }
+
+            // Print transfer progress
+            double progess_percent =  e.FileProgress * 100;
+            // Use "Done" instead of 100%
+            if (progess_percent == 100) Transfer_Progress_List.Items[currentindex].SubItems[1].Text = "Done";
+            else Transfer_Progress_List.Items[currentindex].SubItems[1].Text = progess_percent + "%";
+
+            // Remember a name of the last file reported
+            _lastFileName = e.FileName;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //Player_1.URL = @"D:\Project49\2019-01-15_16-19-33.ts";
+            //Player_1.Ctlcontrols.play();
+
+            // Media Player Initiation
+            Player_1.Ctlenabled = false;
+            Player_1.uiMode = "none";
+            
+            // Transfer Progress List Settings
+            Transfer_Progress_List.Groups.Add(Initiation_Group);
+            Transfer_Progress_List.Groups.Add(Transfer_Group);
+            Transfer_Progress_List.Sorting = SortOrder.None;
+            Transfer_Progress_List.View = View.Details;
+            Transfer_Progress_List.AllowColumnReorder = false;
+            Transfer_Progress_List.FullRowSelect = true;
+            Transfer_Progress_List.Columns.Add(new ColumnHeader());
+            Transfer_Progress_List.Columns[0].Text = "Name";
+            Transfer_Progress_List.Columns[0].Width = 170;
+            Transfer_Progress_List.Columns.Add(new ColumnHeader());
+            Transfer_Progress_List.Columns[1].Text = "Progress";
+            Transfer_Progress_List.Columns[1].Width = 75;
+            Transfer_Progress_List.Items.Add(new ListViewItem(new string[] { "Remote Device Access", "Unavailable" }, Initiation_Group));
+            Transfer_Progress_List.Items.Add(new ListViewItem(new string[] { "File Tranfer Stream", "Stopped" }, Initiation_Group));
+            Transfer_Progress_List.Items.Add(new ListViewItem(new string[] { "Recording Status", "Stopped" }, Initiation_Group));
+
+            //MessageBox.Show(PiCamClient.Properties.Resources.ResourceManager.GetString("xx"));
+        }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            //Player_1.URL = @"D:\Project49\PiCam_LOGO.png";
+            //Player_1.URL = PiCamClient.Properties.Resources.PiCam_LOGO.png;
+            Player_1.Ctlcontrols.pause();
+        }
+
+        private void Player_1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void Button_Connect_Click(object sender, EventArgs e)
+        {
+            status_check_stop_flag = false;
+            //Button_Connect.Enabled = false;
+            //Button_Disconnect.Enabled = true;
+            await Task.Run((Action)status_check_function);
+            //Button_Connect.Enabled = true;
+            //Button_Disconnect.Enabled = false;
+        }
+
+
+        private void Button_Disconnect_Click(object sender, EventArgs e)
+        {
+            status_check_stop_flag = true;
+        }
+
+        private void status_check_function()
+        {
+            status = "ing";
+            //Stuff Happens
+            try
+            {
+                using (Session ssh_session = new Session())
+                {
+                    // ====================================================================================================================
+                    // Select and Establish Connection
+                    switch (PiCam_Selection)
+                    {
+                        case 0:
+                            ssh_session.Open(Eli_HA);
+                            break;
+                        case 1:
+                            ssh_session.Open(PiCam_1);
+                            break;
+                        //case -1:
+                            //return;
+                    }
+
+                    status = "on";
+
+                    while (status_check_stop_flag == false)
+                    {
+                        switch(action)
+                        {
+                            // ====================================================================================================================
+                            // Transfer Recording
+                            case "transfer":
+                                transfer_status = true;
+                                // Read 
+                                /*
+                                RemoteDirectoryInfo directory = ssh_session.ListDirectory("/home/pi/picam/rec/");
+                                
+                                foreach (RemoteFileInfo FileInfo in directory.Files)
+                                {
+                                    uint file_count = 0;
+                                    // Grab file name
+                                    string[] filename_array = FileInfo.Name.Split('.');
+
+                                    // File Type Matching
+                                    if (filename_array[filename_array.Length - 1].ToLower() == "ts") // change to TS for PiCam!!!!!!!!!
+                                    {
+                                        int i = 0;
+                                        string[] Byte_Units = { " B", " KB", " MB", " GB", "TB" };
+                                        long FileSize = FileInfo.Length;
+                                        while (FileSize >= 1024)
+                                        {
+                                            FileSize = FileSize / 1024;
+                                            i++;
+                                        }
+                                        string filename = FileInfo.Name + "(" + FileSize + Byte_Units[i] + ")";
+                                        //Add_File(filename);
+                                        File_List[file_count] = filename;
+                                        file_count++;
+                                    }
+                                }
+                                file_list_state = true;
+                                while (file_list_state) ;*/
+
+                                // Will continuously report progress of transfer
+                                ssh_session.FileTransferProgress += SessionFileTransferProgress;
+                                // Transfer Files
+                                TransferOptions sftp_option = new TransferOptions();
+                                sftp_option.TransferMode = TransferMode.Binary;
+                                TransferOperationResult sftp_result;
+                                //sftp_result = ssh_session.PutFiles(@"D:\Downloads\Bla Bla Bla\Pokemon\*", "/home/pi/Pictures/", false, sftp_option);
+                                sftp_result = ssh_session.GetFiles("/home/pi/picam/rec/*.ts", @"D:\Project49\", false, sftp_option);
+                                transfer_status = false;
+                                //sftp_result.Check();
+
+                                MessageBox.Show("Transfer Done", "Notification", MessageBoxButtons.OK);
+                                action = "none";
+                                break;
+                            case "record":
+                                execute_result = ssh_session.ExecuteCommand("sudo touch ~/picam/hooks/start_record");
+                                action = "none";
+                                break;
+                            case "stop":
+                                execute_result = ssh_session.ExecuteCommand("sudo touch ~/picam/hooks/stop_record");
+                                action = "none";
+                                break;
+                            case "test":
+                                //execute_result = ssh_session.ExecuteCommand("sudo rm -rf /home/pi/picam/rec");
+                                //execute_result = ssh_session.ExecuteCommand("sudo chmod +x /home/pi/Project49/Scripts/TestScript.sh");
+                                execute_result = ssh_session.ExecuteCommand("sudo sh /home/pi/Project49/Code/fivesec.sh");
+                                MessageBox.Show("Test Performed", "Notification", MessageBoxButtons.OK);
+                                action = "none";
+                                //Button_Test.Enabled = true;
+                                break;
+                            // ====================================================================================================================
+                            case "none":
+                                break;
+                        }
+
+                        // ====================================================================================================================
+                        // Check Recording Status
+                        execute_result = ssh_session.ExecuteCommand("sudo cat ~/picam/state/record");
+                        if (execute_result.Output == "false") recording_status = false;
+                        else if (execute_result.Output == "true") recording_status = true;
+                    }
+                    status = "off";
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error Message", MessageBoxButtons.OK);
+            }
+        }
+        
+        private void Status_Refresh_Timer_Tick(object sender, EventArgs e)
+        {
+            switch(status)
+            {
+                case "off":
+                    Button_Connect.Enabled = true;
+                    Button_Disconnect.Enabled = false;
+                    Transfer_Progress_List.Items[0].SubItems[1].Text = "Unavailable";
+                    break;
+                case "ing":
+                    Button_Connect.Enabled = false;
+                    Button_Disconnect.Enabled = false;
+                    Transfer_Progress_List.Items[0].SubItems[1].Text = "Establishing";
+                    break;
+                case "on":
+                    Button_Connect.Enabled = false;
+                    Button_Disconnect.Enabled = true;
+                    Transfer_Progress_List.Items[0].SubItems[1].Text = "Connected";
+                    break;
+            }
+            if (recording_status)
+            {
+                if (action == "stop") Button_Record.Enabled = false;
+                else Button_Record.Enabled = true;
+                Transfer_Progress_List.Items[2].SubItems[1].Text = "Recording";
+                Button_Record.Text = "Stop";
+            }
+            else
+            {
+                if (action == "record") Button_Record.Enabled = false;
+                else Button_Record.Enabled = true;
+                Transfer_Progress_List.Items[2].SubItems[1].Text = "Stopped";
+                Button_Record.Text = "Record";
+            }
+            if(transfer_status)
+            {
+                Button_Transfer.Enabled = false;
+                Transfer_Progress_List.Items[1].SubItems[1].Text = "In Progress";
+            }
+            else
+            {
+                Button_Transfer.Enabled = true;
+                Transfer_Progress_List.Items[1].SubItems[1].Text = "Down";
+            }
+            if(file_list_state)
+            {
+                foreach(string filename in File_List)
+                {
+                    Transfer_Progress_List.Items.Add(new ListViewItem(new string[] { filename, "Waiting" }, Transfer_Group));
+                    Transfer_Progress_List.Items[Transfer_Progress_List.Items.Count - 1].EnsureVisible();
+                }
+                file_list_state = false;
+            }
+        }
+
+        private void PiCam_List_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PiCam_Selection = PiCam_List.SelectedIndex;
+        }
+
+        private void Button_Record_Click(object sender, EventArgs e)
+        {
+            if(recording_status) action = "stop";
+            else action = "record";
+        }
+
+        private void Button_Test_Click(object sender, EventArgs e)
+        {
+            action = "test";
+            //Button_Test.Enabled = false;
+        }
+
+    }
+}
