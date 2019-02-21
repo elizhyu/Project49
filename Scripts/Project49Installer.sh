c141c9d2c9f0fde21ad4178e005f2f61704053be
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-02-20 23:45:59 -0500

    Update Project49Installer.sh

--- Scripts/Project49Installer.sh
+++ Scripts/Project49Installer.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 #This is the script that runs all the installations and sets everything up
+#This has to be run with sudo ./Project49Installer.sh
+
 
 echo "Running the main update/upgrade"
 chmod +x mainUpdater.sh
 ./mainUpdater.sh
+
+echo "Turning on Camera/I2C/SSH"
+chmod +x Utilities.sh
+./Utilities.sh
+
+echo "Adding Networks"
+chmod +x Networks.sh
+./Networks.sh
 
 echo "Installing wiringPi"
 chmod +x wiringPiInstaller.sh
@@ -18,3 +28,7 @@ cd
 cd Project49
 cd Code
 make -B
+
+
+#we have to reboot to complete the changes
+reboot
