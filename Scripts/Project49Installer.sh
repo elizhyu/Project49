5edad1cf750ed20ef2da959ca8378c4382145b0a
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-02-27 19:35:11 -0500

    Update Project49Installer.sh

--- Scripts/Project49Installer.sh
+++ Scripts/Project49Installer.sh
@@ -7,14 +7,22 @@
 echo "Running the main update/upgrade"
 chmod +x mainUpdater.sh
 ./mainUpdater.sh
+chmod -x mainUpdater.sh
 
 echo "Turning on Camera/I2C/SSH"
 chmod +x Utilities.sh
 ./Utilities.sh
+chmod -x Utilities.sh
 
 echo "Adding Networks"
 chmod +x Networks.sh
 ./Networks.sh
+chmod -x Networks.sh
+
+echo "Removing old installations"
+chmod +x Remover.sh
+./Remover.sh
+chmod -x Remover.sh
 
 echo "Installing wiringPi"
 chmod +x wiringPiInstaller.sh
@@ -24,10 +32,10 @@ echo "Installing picam"
 chmod +x piCamInstaller.sh
 ./piCamInstaller.sh
 
-cd
-cd Project49
-cd Code
-make -B
+echo "Making the C program"
+chmod +x ./Maker.sh
+./Maker.sh
+chmod -x ./Maker.sh
 
 
 #we have to reboot to complete the changes
