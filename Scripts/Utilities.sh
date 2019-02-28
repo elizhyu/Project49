eefd2b483a8d1c30408839846d6b96242c497cc3
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-02-27 19:36:35 -0500

    Update Utilities.sh

--- Scripts/Utilities.sh
+++ Scripts/Utilities.sh
@@ -14,16 +14,31 @@ if grep -q 'i2c-dev' /etc/modules; then
 else
   echo 'i2c-dev' >> /etc/modules
 fi
+
+#Turn on dtparam=12c1, not sure if it's needed already tho
 if grep -q 'dtparam=i2c1=on' /boot/config.txt; then
   echo 'Seems i2c1 parameter already set, skip this step.'
 else
-  echo 'dtparam=i2c1=on' >> /boot/config.txt
+	echo "" >> /boot/config.txt
+	echo 'Some parameter for I2C' >> /boot/config.txt
+	echo 'dtparam=i2c1=on' >> /boot/config.txt
 fi
-if grep -q 'dtparam=i2c_arm=on' /boot/config.txt; then
-  echo 'Seems i2c_arm parameter already set, skip this step.'
+
+
+#rewrite the code to reflect how I want it
+if grep -q '#dtparam=i2c_arm=on' /boot/config.txt; then
+	sed -i "s/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g" /boot/config.txt
+        echo "I2C turned on in boot"
+elif grep "dtparam=i2c_arm=off" /boot/config.txt
+then
+        sed -i "s/dtparam=i2c_arm=off/dtparam=i2c_arm=on/g" /boot/config.txt
+
 else
-  echo 'dtparam=i2c_arm=on' >> /boot/config.txt
+	sed -i "s/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g" /boot/config.txt
+        echo "I2C turned on in boot"
 fi
+
+#Another I2C thing needed
 if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
   sed -i 's/^blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
   sed -i 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
@@ -48,9 +63,17 @@ systemctl start ssh
 grep "start_x=1" /boot/config.txt
 if grep "start_x=1" /boot/config.txt
 then
-        exit
-else
-        sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
-        #reboot
+        echo "Camera is already set up"
+elif grep "start_x=0" /boot/config.txt
+then
+	sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
+	echo "Camera is initialized" 
+else #not already set up and no start_x=0
+	echo "" >> /boot/config.txt
+	echo "#Adding the camera functions" >> /boot/config.txt
+	echo "start_x=1" >> /boot/config.txt
+	echo "Camera is initailzed"
 fi
-exit
+
+#reboot
+
