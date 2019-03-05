186c1a57402e8e0901202b136380442f504768f6
jpats523 <44264857+jpats523@users.noreply.github.com>
2019-03-05 17:02:17 -0500

    Update fivesec.sh

--- Code/fivesec.sh
+++ Code/fivesec.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#creates a 5 second recording named preview.sh and puts it in the preview folder inside of picam
 
-touch /home/pi/picam/hooks/start_record
+echo -e "dir=/home/pi/picam/preview\nfilename=preview.ts" > /home/pi/picam/hooks/start_record
 
 sleep 5
 
